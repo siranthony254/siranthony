23ad9b7c440500b821c62602831930cff1285bbb
siranthony254 <officialsiranthony@gmail.com>
2026-04-21 14:23:49 +0300

    modified:   src/sanity/lib/image.ts

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,9 +1,10 @@
-import { createBuilder, type SanityImageSource } from '@sanity/image-url'
+import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url'
 
 import { dataset, projectId } from '../env'
 
 // https://www.sanity.io/docs/image-url
-const builder = createBuilder({ projectId, dataset })
+const builder = imageUrlBuilder({ projectId, dataset })
 
 export const urlFor = (source: SanityImageSource) => {
   return builder.image(source)
